f3c06a8e297c3881fd9df931c0e7217e1d0ae28a
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-27 17:42:49 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  counter = 0
+  new_collection = []
+  if collection == [] 
+    new_collection = []
+  else 
+    collection_count = collection.size  
+    while counter < collection_count
+      yield collection[counter]
+      new_collection << collection[counter]
+      counter += 1
+    end
+  end
+  new_collection
 end
+
+my_each(1) {|i| "#{i}"}
